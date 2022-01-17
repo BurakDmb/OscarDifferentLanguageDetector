f115abae711a40584aa9269d2e26021659c5d210
Burak Han Demirbilek <burakhan.demirbilek@gmail.com>
2022-01-17 21:18:36 +0300

    Updated RF params

--- train_random_forest_and_save.py
+++ train_random_forest_and_save.py
@@ -8,8 +8,8 @@ from pyspark.sql.types import StructType, StructField, DoubleType
 conf = SparkConf()
 conf.setMaster("local[*]").setAppName("CENG790-Project")
 conf.set("spark.driver.memory", "50g")
-conf.set("spark.executor.memory", "2g")
-conf.set("spark.driver.maxResultSize", "0")
+# conf.set("spark.executor.memory", "2g")
+# conf.set("spark.driver.maxResultSize", "0")
 
 spark = SparkSession.builder.config(conf=conf).getOrCreate()
 spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
@@ -25,7 +25,7 @@ rescaledData.show(10)
 (trainingData, testData) = rescaledData.select("label", "features").randomSplit([0.8, 0.2], seed=0)
 
 # RF classifier with default parameters
-rf = RandomForestClassifier(featuresCol="features", labelCol="label", maxDepth=30, maxBins=128, numTrees=100)
+rf = RandomForestClassifier(featuresCol="features", labelCol="label", maxDepth=5, maxBins=32, numTrees=2)
 
 # Fit the model
 rfModel = rf.fit(trainingData)
